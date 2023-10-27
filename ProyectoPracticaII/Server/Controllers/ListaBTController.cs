74e23b41ffb9ab98be2cba36738134ee95bac4d3
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-10-27 10:31:00 -0300

    bUSCAR TALLER AL 50%

---
+++ ProyectoPracticaII/Server/Controllers/ListaBTController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoPracticaII.Client.Models;
+
+namespace ProyectoPracticaII.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListaBTController : ControllerBase
+    {
+
+        private readonly Motored01Context _Context;
+
+        public ListaBTController(Motored01Context motored01Context)
+        {
+            _Context = motored01Context;
+        }
+
+        
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<Taller>> GetTallerPorNombre(string nombre)
+        {
+            try
+            {
+                var taller = await _Context.Tallers
+                    .FirstOrDefaultAsync(t => t.NombreTaller == nombre);
+
+                if (taller == null)
+                {
+                    // Aquí puedes establecer una variable de estado o mostrar un mensaje al usuario
+                }
+
+                return taller;
+            }
+            catch (Exception ex)
+            {
+                // Aquí puedes manejar cualquier excepción no controlada, por ejemplo, registrándola o mostrando un mensaje de error genérico.
+
+                return NotFound();
+            }
+
+
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<List<Motocicleta>>> GetMotocicleta(int id)
+        {
+            var lista = await _Context.Motocicletas.Where(e => e.IdUsuario == id).ToListAsync();
+            return Ok(lista);
+        }
+    }
+}
---
+++ ProyectoPracticaII/Server/Controllers/OpinionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoPracticaII.Client.Models;
+
+namespace ProyectoPracticaII.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpinionController : ControllerBase
+    {
+        private readonly Motored01Context motored01Context; 
+
+        public OpinionController(Motored01Context motored01Context)
+        {
+            this.motored01Context = motored01Context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Taller>>> Get()
+        {
+            var resp = await motored01Context.Tallers.ToListAsync();
+            return resp;
+        }
+
+      
+
+        private async Task<List<Opinione>> GetDbOpiniones()
+        {
+            return await motored01Context.Opiniones.ToListAsync();
+        }
+    }
+
+
+  
+}

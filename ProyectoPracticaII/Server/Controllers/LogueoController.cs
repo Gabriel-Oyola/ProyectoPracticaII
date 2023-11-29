22d538191ade86db7cceaa57b8de930b8a52f7ca
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-11-29 11:58:16 -0300

    Login y registro andando

---
+++ ProyectoPracticaII/Server/Controllers/LogueoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoPracticaII.Client.Models;
+using System.Security.Claims;
+
+namespace ProyectoPracticaII.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogueoController : ControllerBase
+    {
+        private readonly Motored01Context _dbContext;
+
+        public LogueoController(Motored01Context dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        [HttpPost]
+        
+        public async Task<ActionResult<Usuario>> IniciarSesion(string correo, string clave)
+        {
+
+            Usuario usuario_encontrado = await GetUsuario(correo, clave);
+
+            if (usuario_encontrado == null)
+            {
+                string error = "No se encontraron coinsidencias";
+
+            }
+
+           
+            return usuario_encontrado;
+        }
+        public async Task<Usuario> GetUsuario(string correo, string clave)
+        {
+            Usuario usuario_encontrado = await _dbContext.Usuarios.Where(u => u.Correo == correo && u.Clave == clave).FirstOrDefaultAsync();
+
+            return usuario_encontrado;
+        }
+
+    }
+}
---
+++ ProyectoPracticaII/Server/Controllers/RegistroController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+using System;
+using ProyectoPracticaII.Client.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoPracticaII.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistroController : ControllerBase
+    {
+        private readonly Motored01Context _context;
+
+        public RegistroController(Motored01Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        
+        public async Task<ActionResult<Usuario>> Registrarse(Usuario modelo)
+        {
+            //modelo.Clave = CalcularHash(modelo.Clave);
+
+
+            _context.Usuarios.Add(modelo);
+            await _context.SaveChangesAsync();
+            return modelo;
+
+
+
+        }
+
+
+        private string CalcularHash(string input)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Convertir la contraseña en bytes
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                // Convertir los bytes a una cadena hexadecimal
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}

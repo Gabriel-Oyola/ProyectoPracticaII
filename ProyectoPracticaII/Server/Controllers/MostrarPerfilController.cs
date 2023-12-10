704af64eafe47f765a0b60f21d6a48b0fc3eec20
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-12-10 18:07:50 -0300

    en proceso de persistencia de idUsuario

--- ProyectoPracticaII/Server/Controllers/MostrarPerfilController.cs
+++ ProyectoPracticaII/Server/Controllers/MostrarPerfilController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProyectoPracticaII.Client.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ProyectoPracticaII.Server.Controllers
 {
@@ -17,10 +19,15 @@ namespace ProyectoPracticaII.Server.Controllers
         }
 
         [HttpGet("{id:int}")]
-        public async Task<ActionResult<List<Usuario>>> GetUsuario(int id)
+        public async Task<ActionResult<Usuario>> GetUsuario(int id)
         {
-            var lista = await motored01Context.Usuarios.Where(e => e.IdUsuario == id).ToListAsync();
-            return Ok(lista);
+            var User = await motored01Context.Usuarios.Where(e => e.IdUsuario == id).FirstOrDefaultAsync();
+            if (User == null)
+            {
+                return NotFound($"No existe el usuario de Id={id}");
+            }
+            ConvertToHexString(User.Clave);
+            return Ok(User);
         }
 
         [HttpPut("{id:int}")]
@@ -32,12 +39,35 @@ namespace ProyectoPracticaII.Server.Controllers
                 return BadRequest("no se encuentra");
             DbObjeto.NombreUsuario = objeto.NombreUsuario;
             DbObjeto.Correo = objeto.Correo;
-            DbObjeto.Clave = objeto.Clave;
+            DbObjeto.Clave = CalcularHash(objeto.Clave);
 
             await motored01Context.SaveChangesAsync();
 
             return Ok(await motored01Context.Usuarios.ToListAsync());
 
+        }
+        public static string ConvertToHexString(string hash)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in hash)
+            {
+                sb.Append(b.ToString("x2")); // Convierte cada byte a su representación hexadecimal
+            }
+            return sb.ToString();
+        }
+        private string CalcularHash(string input)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
     }
 }

5a35a59f18b5695a65af55fbe38fee716c61b298
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-12-11 17:43:56 -0300

    Redireccionamiento opinion andando

--- ProyectoPracticaII/Server/Controllers/MostrarPerfilController.cs
+++ ProyectoPracticaII/Server/Controllers/MostrarPerfilController.cs
@@ -64,7 +64,7 @@ namespace ProyectoPracticaII.Server.Controllers
         public async Task<ActionResult> Login([FromBody] LoginDTO login)
         {
             SesionDTO sesionDTO = new SesionDTO();
-            ULT email = new ULT();
+           
 
             Usuario usuario_encontrado = await GetUsuario(login.Correo, CalcularHash(login.Clave));
 
@@ -73,7 +73,7 @@ namespace ProyectoPracticaII.Server.Controllers
                 sesionDTO.Nombre = usuario_encontrado.NombreUsuario;
                 sesionDTO.Correo = login.Correo;
                 sesionDTO.Rol = "UsuarioRegistrado";
-                email.GuardarDato(login.Correo);
+             
             }
           
 
